f0bcf72007c68cf9359f39e623bc44e2be526ec8
Tomeek92 <98541483+Tomeek92@users.noreply.github.com>
2024-04-26 15:03:40 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "Andrzej",
+          name: "Skonieczny",
+          phone: "1234567",
+          email: "AndrzejSkonczeny@wp.pl",
+        },
+        {
+          id: "Pietrek",
+          name: "Skonieczny",
+          phone: "1234567",
+          email: "PietrekSkonczeny@wp.pl",
+        },
+      ],
+    };
+  },
+};
+</script>
